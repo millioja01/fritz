20322106fd91304e4f7808270f3ec606efa8896d
Jack <millioja01@esj.org>
2023-11-04 21:26:16 -0400

    add initial height, velocity, and angle

--- main.gd
+++ main.gd
@@ -6,6 +6,10 @@ const e := 2.7182818284
 @onready var object_x_offset: float = $Object.position.x
 @onready var dog_x_offset: float = $Dog.position.x
 
+var initial_velocity := 2.26
+var initial_angle := deg_to_rad(45)
+var initial_height := 1.0
+
 var gravity := 9.8 # g
 var volume := 0.0052 # l
 var drag_coefficient := 0.47 # Cd
@@ -14,8 +18,12 @@ var simulation_time := 0.0 # t
 var tracking_constant := -0.03
 
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("hide"):
+		$CanvasLayer/Control.visible = not $CanvasLayer/Control.visible
+
+
 func _physics_process(delta: float) -> void:
-	%TimeSlider.step = delta
 	if %TimeCheckBox.button_pressed:
 		simulation_time += delta * %TimeMultiplier.text.to_float()
 		if simulation_time > %TimeSlider.max_value:
@@ -47,29 +55,33 @@ func set_object_velocity(x_velocity: float, y_velocity: float) -> void:
 
 func get_x_position(time: float) -> float:
 	var k := (-drag_coefficient * area_of_cross_section) / (2.0 * volume)
-	var c2 := -1.6 / k
+	var n := initial_velocity * cos(initial_angle)
+	var c2 := -n / k
 	
-	return (1.6 / k) * pow(e, k * time) + c2
+	return (n / k) * pow(e, k * time) + c2
 
 
 func get_x_velocity(time: float) -> float:
+	var v := initial_velocity * cos(initial_angle)
 	var k := (-drag_coefficient * area_of_cross_section) / (2.0 * volume)
 	
-	return 1.6 * pow(e, k * time)
+	return v * pow(e, k * time)
 
 
 func get_y_position(time: float) -> float:
 	var k := (-drag_coefficient * area_of_cross_section) / (2.0 * volume)
+	var m := initial_velocity * sin(initial_angle)
+	var c1 := initial_height - m - (gravity / k)
 	
-	var c1 := -(gravity / k + 0.6)
-	
-	return 1.6 * pow(e, time) + (gravity / k) * pow(e, time) - (gravity * time) / k + c1
+	return m * pow(e, time) + gravity * pow(e, time) / k - gravity * time / k + c1
+#	return 1.6 * pow(e, time) + (gravity / k) * pow(e, time) - (gravity * time) / k + c1
 
 
 func get_y_velocity(time: float) -> float:
+	var v := initial_velocity * sin(initial_angle)
 	var k := (-drag_coefficient * area_of_cross_section) / (2.0 * volume)
 	
-	return ((1.6 * k + gravity) * pow(e, time) - gravity) / k
+	return ((k * v + gravity) * pow(e, time) - gravity) / k
 
 
 func set_dog_position(x_position: float) -> void:
@@ -107,43 +119,64 @@ func get_sum_of_dog_change(delta: float, time: float) -> float:
 	return sum_of_change
 
 
-func _on_volume_edit_text_submitted(new_text: String) -> void:
+func _on_volume_edit_text_changed(new_text: String) -> void:
 	if new_text.to_float() == 0.0:
 		return
 	
 	volume = new_text.to_float()
 
 
-func _on_drag_edit_text_submitted(new_text: String) -> void:
+func _on_drag_edit_text_changed(new_text: String) -> void:
 	if new_text.to_float() == 0.0:
 		return
 	
 	drag_coefficient = new_text.to_float()
 
 
-func _on_cross_section_area_text_submitted(new_text: String) -> void:
+func _on_cross_section_area_text_changed(new_text: String) -> void:
 	if new_text.to_float() == 0.0:
 		return
 	
 	area_of_cross_section = new_text.to_float()
 
 
-func _on_object_edit_text_submitted(new_text: String) -> void:
+func _on_object_edit_text_changed(new_text: String) -> void:
 	if new_text.to_float() == 0.0:
 		return
 	
 	object_x_offset = new_text.to_float()
 
 
-func _on_dog_edit_text_submitted(new_text: String) -> void:
+func _on_dog_edit_text_changed(new_text: String) -> void:
 	if new_text.to_float() == 0.0:
 		return
 	
 	dog_x_offset = new_text.to_float()
 
 
-func _on_tracking_edit_text_submitted(new_text: String) -> void:
+func _on_tracking_edit_text_changed(new_text: String) -> void:
 	if new_text.to_float() == 0.0:
 		return
 	
 	tracking_constant = new_text.to_float()
+
+
+func _on_velocity_edit_text_changed(new_text: String) -> void:
+	if new_text.to_float() == 0.0:
+		return
+	
+	initial_velocity = new_text.to_float()
+
+
+func _on_angle_edit_text_changed(new_text: String) -> void:
+	if new_text.to_float() == 0.0:
+		return
+	
+	initial_angle = deg_to_rad(new_text.to_float())
+
+
+func _on_height_edit_text_changed(new_text: String) -> void:
+	if new_text.to_float() == 0.0:
+		return
+	
+	initial_height = new_text.to_float()
